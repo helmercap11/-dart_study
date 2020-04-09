179149a67840dc7405412e426f7c8017546faeab
helmer <helmer>
2020-04-09 17:12:28 +0100

    update study dart

---
+++ index.dart
@@ -0,0 +1,11 @@
+void main(){
+
+  Map usuario = {
+    'nome': 'Helmer Capassola',
+     24: 'vinte e quatro anos',
+    'altura': 1.73,
+    'Github': 'helmerca11',
+   
+  };
+  print(usuario);
+}
